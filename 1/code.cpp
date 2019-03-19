dd81401b769dac76ba4aa99ebf42046beaa06ad5
Alexandrov Nikita <nik.alexandro2013@yandex.ru>
2019-03-19 19:05:18 +0300

    Исправлен стиль кода

--- 1/code.cpp
+++ 1/code.cpp
@@ -2,13 +2,13 @@
 #include "numbers.dat" // Data - массив, Size - размер массива
 
 __attribute__((always_inline)) bool is_prime(int n) {
-    if(n != 1 && n != 2 && n%2 != 0) {
+    if(n != 1 && n != 2 && n % 2 != 0) {
         for(int i = 3; i * i <= n; i += 2)
-            if(n%i == 0)
+            if(n % i == 0)
                 return false;
     }else if(n == 2)
         return true;
-    else if(n == 1 || n%2 == 0)
+    else if(n == 1 || n % 2 == 0)
         return false;
     return true;
 }
@@ -18,12 +18,12 @@ int prime_counter(int left, int right) {
     for(int i = 0; i < Size && Data[i] <= right; i++) {
         if(Data[i] >= left && is_prime(Data[i])) {
             counter++;
-            while(i < Size-1 && Data[i+1] == Data[i]){
+            while(i < Size - 1 && Data[i+1] == Data[i]){
                 i++;
                 counter++;
             }
         } else {
-            while(i < Size-1 && Data[i+1] == Data[i]) {
+            while(i < Size - 1 && Data[i+1] == Data[i]) {
                 i++;
             }
         }
@@ -32,8 +32,8 @@ int prime_counter(int left, int right) {
 }
 
 int main(int argc, char* argv[]) {
-    if(argc != 1 && argc%2 != 0) {
-        for(int i = 1; i < argc; i+=2)
+    if(argc != 1 && argc % 2 != 0) {
+        for(int i = 1; i < argc; i += 2)
             std::cout << prime_counter(std::atoi(argv[i]), std::atoi(argv[i+1])) << std::endl;
         return 0;
     }
