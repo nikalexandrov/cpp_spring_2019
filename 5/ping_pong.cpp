133653b3b4e7976bdb66f78ee35faba210390d30
Alexandrov Nikita <nik.alexandro2013@yandex.ru>
2019-04-18 09:10:27 +0300

    ping pong (multithreading) added

---
+++ 5/ping_pong.cpp
@@ -0,0 +1,27 @@
+#include <thread>
+#include <atomic>
+#include <chrono>
+#include <iostream>
+
+static const int max_count = 500000;
+
+std::atomic<bool> fl;
+
+void ping_pong(const char *str, bool value) {
+    for(int i = 0; i < max_count; i++) {
+        while(fl == value);
+        std::cout << str << std::endl;
+        fl = value;
+    }
+}
+
+int main() {
+    fl = false;
+
+    std::thread t1(ping_pong, "ping", true);
+    std::thread t2(ping_pong, "pong", false);
+
+    t2.detach();
+    t1.join();
+return 0;
+}
